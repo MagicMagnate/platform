f98bceb6be40d118495e972612d55a8004e11f94
Ardhi <ardhi@Macs-Mac-mini.local>
2018-03-03 21:11:39 +0700

    initial phoenix platform application

---
+++ test/platform_web/controllers/page_controller_test.exs
@@ -0,0 +1,9 @@
+defmodule PlatformWeb.PageControllerTest do
+  use PlatformWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Players"
+  end
+
+end
