8b69f0e44c353dfd21f057b0e4e108848c05dfd2
Ardhi <ardhi@Macs-Mac-mini.local>
2018-03-03 23:31:34 +0700

    update production

--- config/prod.exs
+++ config/prod.exs
@@ -16,13 +16,13 @@ use Mix.Config
 config :platform, PlatformWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "platform-magicmagnate.herokuapp.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
-  secret_key_base : System . get_env ( "SECRET_KEY_BASE")
-config :platform , Platform.Repo ,
-adapter : Ecto.Adapters.Postgres ,
-url : System . get_env ( "DATABASE_URL" ),
-pool_size : String . to_integer ( System . get_env("POOL_SIZE") || "10"),
-ssl: true
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base : System.get_env("SECRET_KEY_BASE")
+config :platform ,Platform.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
   # Do not print debug messages in production
 config :logger, level: :info
 
